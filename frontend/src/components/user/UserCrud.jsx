0e95b8ac15b47cefa2a69e8a72cdf9bd02ca54ba
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-04-05 00:07:37 -0300

    Criação do componente USERCRUD

---
+++ frontend/src/components/user/UserCrud.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Component } from "react";
+import Main from '../template/Main'
+
+
+const headerProps = {
+    icon:'users',
+    title: "Usuários",
+    subititle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir.'
+}
+
+export default class UserCrud extends Component {
+
+    render(){
+        return (
+            <Main {...headerProps}>
+                Cadastro de Usuário
+            </Main>
+        )
+    }
+}
