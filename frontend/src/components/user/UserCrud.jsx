d8c030ffbd51eb24035c84f85e79162305d0b511
LucasMatheus96 <lucasmatheus.martinss@gmail.com>
2025-04-07 23:49:43 -0300

    Dia

--- frontend/src/components/user/UserCrud.jsx
+++ frontend/src/components/user/UserCrud.jsx
@@ -20,6 +20,12 @@ export default class UserCrud extends Component {
 
     state = { ...initialState }
 
+    componentWillUnmount(){
+        axios(baseUrl).then(resp=> {
+            this.setState({list:resp.data})
+        })
+    }
+
     clear(){
         this.setState({ user: initialState.user })
     }
